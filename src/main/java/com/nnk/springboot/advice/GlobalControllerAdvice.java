23d71ee0858c13361bec28d11b946a07f19c292b
Matthieu Chollet <matthieuchollet@MacBook-Pro-de-Matthieu.local>
2024-05-19 18:25:12 +0200

    feat: add global controller advice to export authenficated username

--- src/main/java/com/nnk/springboot/advice/GlobalControllerAdvice.java
+++ src/main/java/com/nnk/springboot/advice/GlobalControllerAdvice.java
@@ -1,2 +1,20 @@
-package com.nnk.springboot.advice;public class GlobalControllerAdvice {
+package com.nnk.springboot.advice;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+
+    @ModelAttribute("username")
+    public String username() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication == null || !authentication.isAuthenticated())
+            return null;
+
+        return authentication.getName();
+    }
 }
